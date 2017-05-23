7de544dba23771fcb01feff1b2fb5ea1d80fad68
gentry-swanri <gentryswanri@yahoo.com>
2017-05-23 20:06:12 +0700

    add menu scene and tweaking camera

---
+++ DigipetClient/Assets/Scripts/MainMenuScript/NewGameScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewGameScript : MonoBehaviour {
+
+    public Button newGame;
+    
+
+	// Use this for initialization
+	void Start () {
+        newGame = this.GetComponent<Button>();
+        newGame.onClick.AddListener(newGameOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void newGameOnClick()
+    {
+        SceneManager.LoadScene("NewGameScene");
+    }
+}
